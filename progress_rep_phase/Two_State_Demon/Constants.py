2a1895ef51b5c0a3536e6d7073258227f2bfc821
Erdem Pülat <erdem.pulat@ug.bilkent.edu.tr>
2025-11-03 22:50:29 +0300

    last deletions

--- progress_rep_phase/Two_State_Demon/Constants.py
+++
@@ -1,5 +0,0 @@
-k_B = 1.0  # Boltzmann constant
-T_C = 1.0  # Temperature of cold reservoir
-T_H = 2.0  # Temperature of hot reservoir
-DeltaE = 1.0  # Energy difference between states
-gamma = 1.0  # rate constant
